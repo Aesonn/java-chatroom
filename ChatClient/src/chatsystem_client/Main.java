5b77b30a44a3fae3e13c1950adf82db32a8d09f1
zero1337 <ab7coffee7@hotmail.com>
2011-04-23 18:49:23 +0800

    using JDK7 new features to set frame opacity and shape, but dunno y shape and opacity cant work together = =

--- ChatClient/src/chatsystem_client/Main.java
+++ ChatClient/src/chatsystem_client/Main.java
@@ -4,6 +4,9 @@
  */
 package chatsystem_client;
 
+import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 import javax.swing.JFrame;
 
 /**
@@ -17,7 +20,10 @@ public class Main {
     public static void main(String[] args) {
 
         LoginUI frame = new LoginUI(clientName);
+        frame.setUndecorated(true);        
         frame.setSize(300, 500);
+        frame.setBackground(new Color (0, 0, 0, 100));
+        frame.setShape(new Ellipse2D.Float (0, 0, 300,500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
